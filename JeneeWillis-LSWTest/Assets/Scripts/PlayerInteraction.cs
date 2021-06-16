94b5e8a16e6455d2031278ddaf39cb75c2fe68c6
WillisServices <85991140+WillisServices@users.noreply.github.com>
2021-06-16 21:42:02 +1000

    Player movement added
    Players can move character by clicking on screen.

---
+++ JeneeWillis-LSWTest/Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Willis_Player
+{
+    public class PlayerInteraction : MonoBehaviour
+    {
+        [SerializeField]
+        private PlayerMovement playerMovementScript;
+        [SerializeField]
+        private LayerMask interactables;
+
+        [SerializeField]
+        private float stopDistance = 2f;
+
+        private void Update()
+        {
+            if (Input.GetMouseButton(1))
+            {
+                Vector3 clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Vector2 rayOrigin = new Vector2(clickPosition.x, clickPosition.y);
+
+                RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.zero, interactables);
+
+                if (hit.collider != null)
+                {
+                    if (Vector2.Distance(transform.position, hit.collider.transform.position) <= stopDistance)
+                    {
+                        playerMovementScript.targetPosition = transform.position;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ JeneeWillis-LSWTest/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Willis_Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField]
+        private float movementSpeed = 5f;
+
+        internal Vector2 targetPosition;
+
+        private bool isMoving = false;
+
+        private void Update()
+        {
+            if (Input.GetMouseButton(1))
+            {
+                GetTargetPosition();
+            }
+
+            if (isMoving)
+            {
+                MovePlayer();
+            }
+        }
+
+        private void GetTargetPosition()
+        {
+            targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            isMoving = true;
+        }
+
+        private void MovePlayer()
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementSpeed * Time.deltaTime);
+
+            if (ConvertToVector2(transform.position) == targetPosition)
+            {
+                isMoving = false;
+            }
+        }
+
+        private Vector2 ConvertToVector2(Vector3 vector3)
+        {
+            return (new Vector2(vector3.x, vector3.y));
+        }
+    }
+}
